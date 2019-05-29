b6f06d8a7fb286a6a1b8846b871a63bb3b53500c
Sandeep <ssd2192@gmail.com>
2019-05-29 10:38:33 -0400

    A-B

--- 739379/739379/Program.cs
+++ 739379/739379/Program.cs
@@ -10,6 +10,73 @@ namespace _739379
     {
         static void Main(string[] args)
         {
+            Manager mn = new Manager();
+            mn.Run();
         }
+    }
+
+    class Manager
+    {
+        public void Run()
+        {
+            A_B1 object_b1 = new A_B1();
+            object_b1.PrimeCheck();
+        }
+    }
+
+    class A_B1
+    {
+
+        public void PrimeCheck()
+        {
+            int counter = 0, sum = 0, n=2;
+           
+            while (counter < 500)
+            {
+                if (checkSum(n) == 0)
+                {
+                    sum += n;
+                    counter++;
+
+                }
+                n++;
+            }
+
+            Console.WriteLine("Sum is {0}", sum);
+            Console.ReadKey();
+            
+
+        }
+        public int checkSum(int num)
+        {
+            int n, i, m = 0, flag = 0;
+            //Console.WriteLine("Enter the number to check Prime: ");
+            //n = int.Parse(Console.ReadLine());
+            m = num / 2;
+
+            for (i = 2; i <= m; i++)
+            {
+                if (num % i == 0)
+                {
+                    //Console.WriteLine("Number is not Prime");
+                    //Console.WriteLine(num);
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                //Console.WriteLine(num);
+                //Console.WriteLine("Number is not Prime");
+            }
+
+            return flag;
+
+        }
+       
+    }
+    class B_B2
+    {
+
     }
 }
